a164ba8bfe757ec674bd2b0c18c7fb978bbae881
Hal <73549429+hal0177@users.noreply.github.com>
2024-08-31 15:28:31 +0100

    testnet config

---
+++ ava.testnet.config.cjs
@@ -0,0 +1,8 @@
+module.exports = {
+    ...require("near-workspaces/ava.testnet.config.cjs"),
+    ...require("./ava.config.cjs")
+}
+
+module.exports.environmentVariables = {
+    TESTNET_MASTER_ACCOUNT_ID: ""
+}
